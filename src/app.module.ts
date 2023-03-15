3dcc84c463ecf3fa7f8830414cffe298d7bdf843
Gerry Larios <gerardo.larios69@gmail.com>
2023-03-15 16:28:11 -0600

    feat: scafold for users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { UsersModule } from './users/users.module';
+import User from './users/entities/user.entity';
+
+import Config from './config';
+
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: Config.getValues().database.name,
+      host: Config.getValues().database.host,
+      password: Config.getValues().database.pass,
+      port: Config.getValues().database.port,
+      username: Config.getValues().database.user,
+      entities: [User],
+    }),
+    UsersModule,
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly dataSource: DataSource) {}
+}
